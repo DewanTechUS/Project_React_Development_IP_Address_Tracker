60dd693a30313de9e1f56739dacd99ebd6ebc1e9
Dewan Mahmud Rocky <rockyit10@gmail.com>
2026-01-02 14:05:16 -0500

    chore: header component content and removerd old file from previous project

---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="header">
+      <h1>Dewan Mahmud Project React Development IP Address Tracker</h1>
+      <p className="muted">Search any IP or domain to see location and map.</p>
+    </header>
+  );
+}
